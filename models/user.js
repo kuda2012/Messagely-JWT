b1238602fa5a8fd458de902e1d21068c59ebbb0f
Kuda <kuda2012@gmail.com>
2020-09-03 23:17:02 -0500

    auth test added

--- models/user.js
+++ models/user.js
@@ -2,7 +2,6 @@ const bcrypt = require("bcrypt");
 const { BCRYPT_WORK_FACTOR } = require("../config");
 const db = require("../db");
 const ExpressError = require("../expressError");
-const jwt = require("jsonwebtoken");
 
 /** User class for message.ly */
 
@@ -14,26 +13,14 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
-    try {
-      if (!username || !password || !first_name || !last_name || !phone) {
-        throw new ExpressError(
-          "Please make sure all fields are entered correctly",
-          400
-        );
-      }
-      const hashedPw = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
-      const result = await db.query(
-        `INSERT INTO users
+    const hashedPw = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const result = await db.query(
+      `INSERT INTO users
        VALUES($1, $2, $3, $4, $5)
-        RETURNING username, password`,
-        [username, hashedPw, first_name, last_name, phone]
-      );
-      return result.rows[0];
-    } catch (error) {
-      if (error.code == "23505") {
-        return new ExpressError("Username taken. Please pick another!", 400);
-      }
-    }
+        RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPw, first_name, last_name, phone]
+    );
+    return result.rows[0];
   }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
@@ -52,16 +39,9 @@ class User {
     );
     if (results.rows[0]) {
       const checkPw = await bcrypt.compare(password, results.rows[0].password);
-      if (checkPw) {
-        return checkPw;
-      } else {
-        return checkPw;
-      }
+      return checkPw;
     } else {
-      return new ExpressError(
-        "Username and Password combination was invalid",
-        400
-      );
+      return false;
     }
   }
 
@@ -79,7 +59,7 @@ class User {
       [username]
     );
     if (results.rows[0]) {
-      return result.rows[0];
+      return results.rows[0];
     } else {
       return new ExpressError("Username provided does not exist", 404);
     }
@@ -89,7 +69,9 @@ class User {
    * [{username, first_name, last_name, phone}, ...] */
 
   static async all() {
-    const results = await db.query(`SELECT * FROM users`);
+    const results = await db.query(
+      `SELECT username, first_name, last_name, phone FROM users`
+    );
     return results.rows;
   }
 
@@ -107,7 +89,7 @@ class User {
       return new ExpressError("Please make sure username is provided", 400);
     }
     const results = await db.query(
-      `SELECT * FROM users 
+      `SELECT username, first_name, last_name, phone, join_at, last_login_at FROM users 
        WHERE username = $1`,
       [username]
     );
@@ -131,22 +113,26 @@ class User {
       return new ExpressError("Please make sure username is provided", 400);
     }
     const results = await db.query(
-      `SELECT id, u.first_name, u.last_name, u.phone, body, sent_at, read_at
+      `SELECT id, u.username, u.first_name, u.last_name, u.phone, body, sent_at, read_at
       FROM messages
       JOIN users AS u
       ON messages.to_username = u.username
       WHERE from_username = $1`,
       [username]
     );
-    results.rows[0].to_user = {
-      first_name: results.rows[0].first_name,
-      last_name: results.rows[0].last_name,
-      phone: results.rows[0].phone,
-    };
-    delete results.rows[0].first_name;
-    delete results.rows[0].last_name;
-    delete results.rows[0].phone;
-    return results.rows[0];
+    results.rows.forEach(function (row, i, arr) {
+      row.to_user = {
+        username: arr[i].username,
+        first_name: arr[i].first_name,
+        last_name: arr[i].last_name,
+        phone: arr[i].phone,
+      };
+      delete arr[i].username;
+      delete arr[i].first_name;
+      delete arr[i].last_name;
+      delete arr[i].phone;
+    });
+    return results.rows;
   }
 
   /** Return messages to this user.
@@ -162,26 +148,30 @@ class User {
       return new ExpressError("Please make sure username is provided", 400);
     }
     const results = await db.query(
-      `SELECT id, u.first_name, u.last_name, u.phone, body, sent_at, read_at
+      `SELECT id, u.username, u.first_name, u.last_name, u.phone, body, sent_at, read_at
       FROM messages
       JOIN users AS u
       ON messages.from_username = u.username
       WHERE to_username = $1`,
       [username]
     );
-    results.rows[0].from_user = {
-      first_name: results.rows[0].first_name,
-      last_name: results.rows[0].last_name,
-      phone: results.rows[0].phone,
-    };
-    delete results.rows[0].first_name;
-    delete results.rows[0].last_name;
-    delete results.rows[0].phone;
-    return results.rows[0];
+    results.rows.forEach(function (row, i, arr) {
+      row.from_user = {
+        username: arr[i].username,
+        first_name: arr[i].first_name,
+        last_name: arr[i].last_name,
+        phone: arr[i].phone,
+      };
+      delete arr[i].username;
+      delete arr[i].first_name;
+      delete arr[i].last_name;
+      delete arr[i].phone;
+    });
+    return results.rows;
   }
 }
 
-const user = User.messagesTo("logiman").then((res) => console.log(res));
+// const user = User.messagesTo("logiman").then((res) => console.log(res));
 // const user = User.register({
 //   username: "kudaman",
 //   password: "dogs",
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,44 @@
+const express = require("express");
+const router = new express.Router();
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const ExpressError = require("../expressError");
+const User = require("../models/user");
+const { SECRET_KEY } = require("../config");
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
 
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw new ExpressError(
+        "Please provide a username and password. Make sure all items are spelled correctly!",
+        400
+      );
+    }
+    const results = await User.authenticate(username, password);
+    if (results) {
+      const token = jwt.sign(
+        { iat: Date.now(), username: username },
+        SECRET_KEY
+      );
+      await User.updateLoginTimestamp(username);
+      return res.json({ username: username, token: token });
+    } else {
+      throw new ExpressError(
+        "Username and Password combination was invalid",
+        400
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -11,3 +46,43 @@
  *
  *  Make sure to update their last-login!
  */
+
+router.post("/register", async (req, res, next) => {
+  try {
+    if (
+      !req.body.username ||
+      !req.body.password ||
+      !req.body.first_name ||
+      !req.body.last_name ||
+      !req.body.phone
+    ) {
+      throw new ExpressError(
+        "Please make sure all fields are entered correctly",
+        400
+      );
+    }
+    const results = await User.register(req.body);
+    if (results) {
+      await User.updateLoginTimestamp(req.body.username);
+      const token = jwt.sign(
+        { iat: Date.now(), username: req.body.username },
+        SECRET_KEY
+      );
+      results.token = token;
+      return res.json(results);
+    } else {
+      throw new ExpressError(
+        "Please ensure all fields are entered correctly",
+        400
+      );
+    }
+  } catch (error) {
+    if (error.code == "23505") {
+      return next(
+        new ExpressError("Username taken. Please pick another!", 400)
+      );
+    }
+    next(error);
+  }
+});
+module.exports = router;
